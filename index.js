66cb7e2f7a03a3601907152a9ca210d6367957b6
amitwev <amitwev@gmail.com>
2020-03-12 23:01:33 +0200

    heroku deploy

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-//running the app 
+//running the app on prod
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+
 app.listen(PORT, function() {
     console.log("App is listening on port = ", PORT, process.env.DB_USER);
 });
